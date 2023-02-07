44f537d9518f24dedb1777251b32a6514ba44861
Tanuja1 Patil <Tanuja1.Patil@ril.com>
2023-02-07 17:16:04 +0530

    linkedlist

---
+++ DataStructure/linkedlist.java
@@ -0,0 +1,30 @@
+
+public class ListNode{
+    int val;
+    ListNode next;
+    ListNode(){
+        
+    }
+    ListNode(int val){
+        this.val=val;
+    }
+    ListNode(int val,ListNode next){
+        this.val=val;
+        this.next=next;
+    }
+}
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        public ListNode reverseList(ListNode head){
+        ListNode newHead=null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next= newHead;
+            newHead= head;
+            head=next;
+        }
+        return newHead;
+        }
+    }
+}
